42993f72f62de0cd33886070a4c1bdd11ab0d3a6
Арсен Тангатаров <arsen01998@yandex.ru>
2022-12-12 18:22:29 +0300

    Task to func component

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,18 +1,17 @@
-import { Component } from 'react'
 import { formatDistanceToNow } from 'date-fns'
 import PropTypes from 'prop-types'
+import { useState, useEffect } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import './Task.css'
 
-class Task extends Component {
-  state = {
-    label: this.props.label,
-    isEdit: false,
-    isDoneId: uuidv4(),
-    timeOut: null
-  }
+const Task = ({time, label, done, onToggleDone, onDeleted, id, editItem, editTime, date}) => {
 
-  static defaultProps = {
+  const[labelLocal, setLabelLocal] = useState(label)
+  const[isEdit, setIsEdit] = useState(false)
+  const[isDoneId] = useState(uuidv4())
+  let timer = null
+
+  Task.defaultProps = {
     label: '',
     done: false,
     onToggleDone: () => {},
@@ -20,7 +19,7 @@ class Task extends Component {
     date: new Date(),
   }
 
-  static propTypes = {
+  Task.propTypes = {
     label: PropTypes.string,
     done: PropTypes.bool,
     onToggleDone: PropTypes.func,
@@ -28,114 +27,106 @@ class Task extends Component {
     date: PropTypes.instanceOf(Date),
   }
 
-  onLabelChange = (e) => {
-    this.setState({
-      label: e.target.value,
-    })
-  }
-
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault()
-    if(this.state.label.trim() !== ''){
-      this.props.editItem( this.props.id, this.state.label.trim() )
-      if(this.state.isEdit) this.setEdit()
+    if(labelLocal.trim() !== ''){
+      editItem( id, labelLocal.trim() )
+      if(isEdit) setEdit()
     }else{
-      this.props.onDeleted(this.props.id)
+      onDeleted(id)
     }
   }
 
-  startTimer = () =>{
-    this.stopTimer()
-    this.setState(()=>{
-      return{
-        timeOut: setInterval(()=>{
-          let time = this.props.time
-          if (time > 0 && !this.props.done) {
-            this.props.editTime(this.props.id, --time)
-          }else this.stopTimer()
-        }, 1000)
-      }
-    })
+  const stopTimer = () => {
+    clearInterval(timer)
   }
 
-  stopTimer = () => {
-    clearInterval(this.state.timeOut)
+  const startTimer = () =>{
+    stopTimer()
+    timer =
+      setInterval(()=>{
+        if (time > 0 && !done) {
+          editTime(id, --time)
+        }else{
+          stopTimer()
+        } 
+      }, 1000)
+    
   }
 
-  setEdit = () => {
-    this.setState({isEdit:!this.state.isEdit})
+  const setEdit = () => {
+    setIsEdit((e)=> !e)
   }
 
-  numberToTime(num){
+  const numberToTime = (num) => {
     const minutes = Math.trunc(num / 60)
     const seconds = num % 60
     return (minutes < 10 ? `0${minutes}`: minutes)  
     +':'+  (seconds < 10 ? `0${seconds}`: seconds)
   }
 
-  componentWillUnmount(){
-    this.stopTimer()
-  }
-
-  render() {
-    const {time, label, done, onToggleDone, onDeleted, id} =
-      this.props
-    let classNames = 'task__description'
-    done ? classNames += ' done' : classNames 
-    let classTaskInput = 'task__edit'
-    let classTaskView = 'task__view'
-
-    if (this.state.isEdit) {
-      classTaskInput += ' edit'
-      classTaskView += ' edit'
+  useEffect(() => {
+    return () => {
+      stopTimer()
     }
+  }, [])
+  
 
-    return (
-      <li className="task">
-        <div className={classTaskView}>
-          <input
-            id={this.state.isDoneId}
-            onChange  ={onToggleDone}
-            className="task__toggle"
-            type="checkbox"
-            defaultChecked={done}
-          />
-          <div className='task__container'>
-            <label htmlFor={this.state.isDoneId} className={classNames}>
-              {label}
-            </label>
-            <span className="task__timer">
-              <button onClick={this.startTimer} className="task__icon-play"></button>
-              <button onClick={this.stopTimer} className="task__icon-pause"></button>
-              <span>{this.numberToTime(time)}</span> 
-            </span>
-            <span className="task__created">
-              {formatDistanceToNow(this.props.date)}
-            </span>
-          </div>
-          <label
-            onClick={this.setEdit}
-            className="icon task__icon-edit"
-            htmlFor={id}
-          ></label>
-          <button
-            onClick={onDeleted}
-            className="icon task__icon-destroy"
-          ></button>
-        </div>
-        <form onSubmit={this.onSubmit}>
-          <input
-            id={id}
-            onBlur={this.onSubmit}
-            onChange={(e)=> this.onLabelChange(e)}
-            value={this.state.label}
-            type="text"
-            className={classTaskInput}
-          />
-        </form>
-      </li>
-    )
+  let classNames = 'task__description'
+  done ? classNames += ' done' : classNames 
+  let classTaskInput = 'task__edit'
+  let classTaskView = 'task__view'
+
+  if (isEdit) {
+    classTaskInput += ' edit'
+    classTaskView += ' edit'
   }
+
+  return (
+    <li className="task">
+      <div className={classTaskView}>
+        <input
+          id={isDoneId}
+          onChange  ={onToggleDone}
+          className="task__toggle"
+          type="checkbox"
+          defaultChecked={done}
+        />
+        <div className='task__container'>
+          <label htmlFor={isDoneId} className={classNames}>
+            {label}
+          </label>
+          <span className="task__timer">
+            <button onClick={startTimer} className="task__icon-play"></button>
+            <button onClick={stopTimer} className="task__icon-pause"></button>
+            <span>{numberToTime(time)}</span> 
+          </span>
+          <span className="task__created">
+            {formatDistanceToNow(date)}
+          </span>
+        </div>
+        <label
+          onClick={setEdit}
+          className="icon task__icon-edit"
+          htmlFor={id}
+        ></label>
+        <button
+          onClick={onDeleted}
+          className="icon task__icon-destroy"
+        ></button>
+      </div>
+      <form onSubmit={onSubmit}>
+        <input
+          id={id}
+          onBlur={onSubmit}
+          onChange={(e)=> setLabelLocal(e.target.value)}
+          value={labelLocal}
+          type="text"
+          className={classTaskInput}
+        />
+      </form>
+    </li>
+  )
 }
 
 export default Task
