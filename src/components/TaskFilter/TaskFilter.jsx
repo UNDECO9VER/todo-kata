6077188e992dc0a0a07f07181a0092f707582bdc
Арсен Тангатаров <arsen01998@yandex.ru>
2022-12-12 17:10:56 +0300

    TaskFilter to func component

--- src/components/TaskFilter/TaskFilter.jsx
+++ src/components/TaskFilter/TaskFilter.jsx
@@ -1,34 +1,31 @@
-import { Component } from 'react'
+// import { useState } from 'react'
 import PropTypes from 'prop-types'
 import './TaskFilter.css'
 
-class TaskFilter extends Component {
-  state ={
-    filter: ['All', 'Active', 'Completed']
-  }
-  static defaultProps = {
+const TaskFilter = ({setFilter}) => {
+  const filter = ['All', 'Active', 'Completed']
+
+  TaskFilter.defaultProps = {
     setFilter: () => {},
   }
 
-  static propTypes = {
+  TaskFilter.propTypes = {
     setFilter: PropTypes.func,
   }
-  render() {
-    const { setFilter } = this.props
-    return (
-      <ul className="task-filer">
-        {
-          this.state.filter.map((item)=>{
-            return(
-              <li key={item}>
-                <button className='sidebar__label' onClick={(e) => setFilter(e.target.value)} value={item.toLocaleLowerCase()}>{item}</button>
-              </li>
-            ) 
-          })
-        }
-      </ul>
-    )
-  }
+
+  return (
+    <ul className="task-filer">
+      {
+        filter.map((item)=>{
+          return(
+            <li key={item}>
+              <button className='sidebar__label' onClick={(e) => setFilter(e.target.value)} value={item.toLocaleLowerCase()}>{item}</button>
+            </li>
+          ) 
+        })
+      }
+    </ul>
+  )
 }
 
 export default TaskFilter
